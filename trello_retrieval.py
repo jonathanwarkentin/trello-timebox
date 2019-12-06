8eb94a9c5e93e24dea7b1daac73aa3da5b3ca4f9
Jonathan <warkentin.jon@gmail.com>
2019-12-06 17:00:24 -0600

    separate functions more, pretty print cards

--- trello_retrieval.py
+++ trello_retrieval.py
@@ -7,6 +7,7 @@ api_key = os.getenv('API_KEY')
 token = os.getenv('TOKEN')
 credentials = 'key=[[' + api_key + ']]&token=[[' + token
 request_template = """https://api.trello.com/1/{}&""" + credentials
+time_est_field_id = os.getenv('TIME_EST_FIELD')
 
 
 # Get user boards
@@ -20,48 +21,67 @@ def get_boards(member_id):
 
 
 def get_cards(board_id):
-    # req = 'boards/' + board_id + '/cards'
     url = 'https://api.trello.com/1/board/' + board_id + '/cards'
     querystring = {"fields":"name,due","members":"false","customFieldItems":"true","key":api_key,"token":token}
     response = requests.request("GET", url, params=querystring)
     all_cards = json.loads(response.text)
-    # print(all_cards)
     due_cards = list(filter(lambda card: card['due'] is not None, all_cards))
+    return due_cards
+
+
+def sort_cards(due_cards):
     sorted_tasks = sorted(
         due_cards,
         key=lambda card: datetime.strptime(card['due'], '%Y-%m-%dT%H:%M:%S.%fZ'), reverse=False
     )
-
     return sorted_tasks
 
 
 def print_cards(tasks):
+    print('____________________________________________________________________________')
+    print('TASK LIST:')
+    print('____________________________________________________________________________')
+    print()
     for i in range(len(tasks)):
-        print(tasks[i]['name'] + ' - DUE: ' + datetime.strptime(tasks[i]['due'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime("%c"))
-        print(tasks[i]['customFieldItems'])
+        print(tasks[i]['name'])
+        print('DUE: ' + datetime.strptime(tasks[i]['due'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime("%c") + ' UTC')
+        time_est = get_time_estimate(tasks[i])
+        hours = time_est / 60
+        if hours < 1:
+            print('TIME ESTIMATE: ' + str(time_est) + ' minutes')
+        else:
+            print('TIME ESTIMATE: ' + str(hours) + ' hours')
+        print()
+    print('____________________________________________________________________________')
+    print()
 
 
 def get_cards_for_days(tasks, days):
-    limit = datetime.now() + timedelta(days=days)
+    limit = datetime.utcnow() + timedelta(days=days)
     relevant_cards = list(filter(lambda card: datetime.strptime(card['due'], '%Y-%m-%dT%H:%M:%S.%fZ') <= limit, tasks))
     return relevant_cards
 
 
+def get_time_estimate(task):
+    return int(next(field for field in task['customFieldItems'] if field['idCustomField'] == time_est_field_id)['value']['number'])
+
+
 def get_total_time_estimate(tasks):
     total = 0
-    hours = 0
-    minutes = 0
     for task in tasks:
-        time_est = next(field for field in task['customFieldItems'] if field['idCustomField'] == '5deac74aa387e46d08bf6520')
-        time_est = time_est['value']['number']
-        total += int(time_est)
+        time_est = get_time_estimate(task)
+        total += time_est
     hours = total / 60
-    minutes = total % 60
-    print('Total time for ' + str(len(tasks)) + ' tasks: ' + str(hours) + ' hours ' + str(minutes) + ' minutes')
+    if hours < 1:
+        print('Total time for ' + str(len(tasks)) + ' tasks: ' + str(total) + ' minutes')
+    else:
+        print('Total time for ' + str(len(tasks)) + ' tasks: ' + str(hours) + ' hours')
     return total
 
 
 if __name__ == "__main__":
     username = os.getenv('USERNAME')
     cards = get_cards(os.getenv('BOARD_ID'))
-    get_total_time_estimate(get_cards_for_days(cards, 7))
+    incoming_cards = get_cards_for_days(cards, 4)
+    print_cards(sort_cards(incoming_cards))
+    get_total_time_estimate(incoming_cards)
